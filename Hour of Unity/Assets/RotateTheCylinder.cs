2523200b03c6da9da2d21f3066cc45e60aeed7f7
Tim Heider <tim@preludevoice.com>
2016-12-02 09:00:16 -0800

    cylinder with rotation script

---
+++ Hour of Unity/Assets/RotateTheCylinder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTheCylinder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Starting my Cylinder rotation script");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(1, 1, 0);
+	}
+}
